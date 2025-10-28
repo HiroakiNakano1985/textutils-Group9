069848aa1205f034d2b0e6662d43d8eb40c6b5d4
engpongtanya-16 <pornpisuth.pongtanya@alumni.esade.edu>
2025-10-28 13:38:37 +0100

    feat(remove_punctuation): add remove_punctuation() and unit tests

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -6,3 +6,12 @@ def word_count(text: str) -> dict[str, int]:
         counts[word] = counts.get(word, 0) + 1
 
     return counts
+
+
+
+import string
+
+def remove_punctuation(text):
+    
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -14,3 +14,24 @@ def test_word_count_extra_spaces():
     text = "hi   hi    hi\nhi"
     result = core.word_count(text)
     assert result == {"hi": 4}
+    
+    
+    
+
+def test_remove_punctuation_basic():
+    from textutils import core
+    text = "Hello, world!"
+    result = core.remove_punctuation(text)
+    assert result == "Hello world"
+
+def test_remove_punctuation_mixed():
+    from textutils import core
+    text = "Hi!!! It's... me???"
+    result = core.remove_punctuation(text)
+    assert result == "Hi Its me"
+
+def test_remove_punctuation_numbers():
+    from textutils import core
+    text = "Wow, 123!!!"
+    result = core.remove_punctuation(text)
+    assert result == "Wow 123"
