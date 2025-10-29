8cdbce9712af72289b0037a44c041efdac8c3da1
Mateo Barreda <mateo.barredariveros@alumni.esade.edu>
2025-10-29 13:48:15 +0100

    Updating test name and fixing previous mistake

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,42 +1,19 @@
-import textutils.core as c
-
-def test_count_vowels_basic():
-    text = "Hello"
-    assert c.count_vowels(text) == 2
-
-def test_count_vowels_multiple_words():
-    text = "Hello professor, how are you?"
-    assert c.count_vowels(text) == 10
-
-def test_count_vowels_capital_vowels():
-    text = "AAAY CARAMBAAA, please help me!"
-    assert c.count_vowels(text) == 13
-
-def test_count_vowels_no_vowels():
-    text = "rhythm"
-    assert c.count_vowels(text) == 0
-
-def test_count_vowels_empty_string():
-    text = ""
-    assert c.count_vowels(text) == 0
-
 import textutils.core as core
 
-def test_reverse_words():
+def test_word_count_basic():
     text = "Red red BLUE"
-    result = c.word_count(text)
+    result = core.word_count(text)
     assert result == {"red": 2, "blue": 1}
 
-def test_remove_punctuation():
+def test_word_count_empty_string():
     text = ""
-    result = c.word_count(text)
+    result = core.word_count(text)
     assert result == {}
 
-def test_word_lengths():
+def test_word_count_extra_spaces():
     text = "hi   hi    hi\nhi"
-    result = c.word_count(text)
+    result = core.word_count(text)
     assert result == {"hi": 4}
-
 
 
 
