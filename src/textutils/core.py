f560a9032eef6fa215b30706cb239da34de14f08
HiroakiNakano1985 <hiroakinakano.hj@gmail.com>
2025-10-30 18:14:30 +0100

    change core.py

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -4,7 +4,6 @@ import string
 from typing import Dict
 
 def unique_words(text):
-    import re
     words = re.findall(r'\b\w+\b', text.lower())
     unique_words = sorted(set(words))
     return unique_words
@@ -17,10 +16,6 @@ def word_count(text: str) -> dict[str, int]:
         counts[word] = counts.get(word, 0) + 1
 
     return counts
-
-
-
-import string
 
 def remove_punctuation(text):
     
