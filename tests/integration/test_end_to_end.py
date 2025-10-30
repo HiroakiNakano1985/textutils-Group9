8ef02e556ecea5e24170ee080b7bc08ce7bfa926
HiroakiNakano1985 <hiroakinakano.hj@gmail.com>
2025-10-30 18:34:42 +0100

    changed test ete

--- tests/integration/test_end_to_end.py
+++ tests/integration/test_end_to_end.py
@@ -1,6 +1,16 @@
 import textutils.core as core
+
+#I don't still understand how each function works. Therefore, result is empty now.
+
 def test_word_lengths_pipeline():
-    text = "Hello,   world!"
+    text = "Hello,   world!\t My name is   Hiroaki Nakano \n We are all friends!!"
     normalized = core.normalize_whitespace(text)
-    result = core.word_lengths(normalized)
-    assert result == {"hello": 5, "world": 5}
+    punctuated = core.remove_punctuation(normalized)
+    counts = core.word_count(punctuated)
+    unique = core.unique_words(punctuated)    
+    length = core.word_lengths(punctuated)
+    vowels = core.count_vowels(punctuated)
+    assert counts ==
+    assert unique ==
+    assert length == {"hello": 5, "world": 5}
+    assert vowels ==
